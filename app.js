876bc5832badac6b69f930bc5755bd59b6a76cb6
brnbp <bruno9pereira@gmail.com>
2017-10-22 02:31:20 -0200

    get status for track

--- app.js
+++ app.js
@@ -45,19 +45,26 @@ yargs
   .command('play', 'play music', () => {}, (argv) => {
     console.log('Playing:')
     exec('osascript -e \'tell application "Spotify" to play\'')
+    getStatus()
    })
   .command('pause', 'pause music', () => {}, (argv) => {
     console.log('Pausing:')
     exec('osascript -e \'tell application "Spotify" to pause\'')
+    getStatus()
    })
    .command('next', 'next music', () => {}, (argv) => {
     console.log('Playing Next:')
     exec('osascript -e \'tell application "Spotify" to next track\'')
+    getStatus()
    })
   .command('prev', 'previous music', () => {}, (argv) => {
     console.log('Playing Previous:')
     exec('osascript -e \'tell application "Spotify" \n set player position to 0  \n  previous track  \n end tell\'')
+    getStatus()
    })
+  .command('status', 'status music', ()=>{}, argv => {
+    getStatus()
+  })
   .argv
 
 
