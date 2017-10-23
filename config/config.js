a9fe7d324141695e04272e977aefaf048293edce
brnbp <bruno9pereira@gmail.com>
2017-10-23 19:16:46 -0200

    update doc guide for api authorization implementation

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports =  {
     // see authorization flow
     // https://developer.spotify.com/web-api/authorization-guide/#authorization-code-flow
-    api_token: 'your-api-token' // see https://developer.spotify.com/web-api/console/get-search-item/
+    // see https://developer.spotify.com/web-api/console/get-search-item/
+    refresh_token: 'your-refresh-token'
 };
