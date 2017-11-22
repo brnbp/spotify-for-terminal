e9b1eb5b4b5c5541cb34a8723009b66bb4ad12aa
brnbp <bruno9pereira@gmail.com>
2017-11-22 20:32:38 -0200

    upgrade actions

--- commands/actions.js
+++ commands/actions.js
@@ -1,24 +1,24 @@
-const exec = require('child_process').exec;
+const osascript = require('node-osascript');
 
 class Actions {
     play() {
         console.log('Playing:')
-        exec('osascript -e \'tell application "Spotify" to play\'')
+        osascript.execute('tell application "Spotify" to play')
     }
 
     pause() {
         console.log('Pausing:')
-        exec('osascript -e \'tell application "Spotify" to pause\'')
+        osascript.execute('tell application "Spotify" to pause')
     }
 
     next() {
         console.log('Playing Next:')
-        exec('osascript -e \'tell application "Spotify" to next track\'')
+        osascript.execute('tell application "Spotify" to next track')
     }
 
     previous() {
         console.log('Playing Previous:')
-        exec('osascript -e \'tell application "Spotify" \n set player position to 0  \n  previous track  \n end tell\'')
+        osascript.execute('tell application "Spotify" \n set player position to 0  \n  previous track  \n end tell')
     }
 }
 
